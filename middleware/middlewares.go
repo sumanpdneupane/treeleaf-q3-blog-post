20c3c51b057661d0b14f97ef4b66104dab2c540b
Suman Neupane <sumanmyon.sam@gmail.com>
2024-11-28 16:57:25 +0545

    [update] seperating jwt token generating from auth controller to middleware

--- middleware/middlewares.go
+++ middleware/middlewares.go
@@ -3,10 +3,42 @@ package middleware
 import (
 	"os"
 	"strings"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
+
+// GenerateJWT generates a JWT token with the given user ID and role
+func GenerateJWT(userID int, role string, expirationHours int) (string, error) {
+	var JWT_SECRET = os.Getenv("JWT_SECRET")
+
+	// Default to 72 hours if no expiration time is provided
+	if expirationHours == 0 {
+		expirationHours = 72
+	}
+
+	// Set expiration time
+	expirationTime := time.Now().Add(time.Duration(expirationHours) * time.Hour).Unix()
+
+	// Create the JWT claims
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"exp":     expirationTime,
+	}
+
+	// Generate the token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token using the secret key
+	signedToken, err := token.SignedString([]byte(JWT_SECRET))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
 
 // AuthMiddleware checks if the user is authenticated via JWT
 func AuthMiddleware(c *fiber.Ctx) error {
